fafc24af45bd6fcbb8afb894f769626aef3bb4c7
xuwenqing <xuwenqing0518@163.com>
2016-04-11 12:49:12 +0800

    增加ConfigurableListableBeanFactory接口

--- src/main/java/tinyspring/framework/beans/support/DefaultListableBeanFactory.java
+++ src/main/java/tinyspring/framework/beans/support/DefaultListableBeanFactory.java
@@ -1,13 +1,16 @@
-package tinyspring.framework.beans.factory.support;
+package tinyspring.framework.beans.support;
 
-import tinyspring.framework.beans.factory.ListableBeanFactory;
-import tinyspring.framework.beans.factory.config.BeanDefinition;
+import tinyspring.framework.beans.config.BeanDefinition;
+import tinyspring.framework.beans.config.ConfigurableListableBeanFactory;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory
-        implements ListableBeanFactory, BeanDefinitionRegistry {
+        implements ConfigurableListableBeanFactory, BeanDefinitionRegistry {
+
+    public DefaultListableBeanFactory() {
+    }
 
     private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>(64);
 
